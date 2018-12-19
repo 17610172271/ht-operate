1bd4dc1a6d312028210c733432aa692dbcb44e8b
DESKTOP-SBHJ9TT\LiKui <601353149@qq.com>
2018-12-19 15:16:53 +0800

    1

--- vue2-admin-lte-master/src/pages/home/home.vue
+++ vue2-admin-lte-master/src/pages/home/home.vue
@@ -6,26 +6,26 @@
                 <div class="container-fluid m-t-md">
                     <div class="row align-bottom flex  p-o-lg">
                         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-2 p-r-sm p-n">
-                            <div class="home-data-item bg-white p-smone bg-green">
-                                <h5 class="m-n"><b>代理商总数</b> <span class="fa fa-info-circle pull-right"></span></h5>
+                            <div class="home-data-item bg-white p-smone bg-blue1">
+                                <h5 class="m-n text-white text-left"><b>代理商总数</b> <span class="fa fa-info-circle pull-right"></span></h5>
                                 <div class="text-xxxlg text-bold p-v-sm">{{data.agent_num || 0}} <span style="margin-left: 15px;"></span>个</div>
                             </div>
                         </div>
                         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-2 p-r-sm p-n">
-                            <div class="home-data-item bg-white p-smone bg-orange">
-                                <h5 class=" m-n">影院总数 <span class="fa fa-info-circle pull-right"></span></h5>
+                            <div class="home-data-item bg-white p-smone bg-green1">
+                                <h5 class="m-n text-white text-left">影院总数 <span class="fa fa-info-circle pull-right"></span></h5>
                                 <div class="text-xxxlg text-bold p-v-sm">{{data.cinema_num || 0}} <span style="margin-left: 15px;"></span>个</div>
                             </div>
                         </div>
                         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-2 p-r-sm p-n">
                             <div class="home-data-item bg-white p-smone bg-red">
-                                <h5 class=" m-n">本月播放次数 <span class="fa fa-info-circle pull-right"></span></h5>
-                                <div class="text-xxxlg text-bold p-v-sm">{{data.month_play_num || 0}} <span style="margin-left: 15px;"></span>个</div>
+                                <h5 class="m-n text-white text-left">本月播放次数 <span class="fa fa-info-circle pull-right"></span></h5>
+                                <div class="text-xxxlg text-bold p-v-sm">{{data.month_play_num || 0}} <span style="margin-left: 15px;"></span>次</div>
                             </div>
                         </div>
                         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-2 p-r-sm p-n">
                             <div class="home-data-item bg-white p-smone bg-yellow">
-                                <h5 class=" m-n">待审核 <span class="fa fa-info-circle pull-right"></span></h5>
+                                <h5 class="m-n text-white text-left">待审核 <span class="fa fa-info-circle pull-right"></span></h5>
                                 <div class="text-xxxlg text-bold p-v-sm">{{data.wait_check_num || 0}} <span style="margin-left: 15px;"></span>个</div>
                             </div>
                         </div>
@@ -34,16 +34,22 @@
             </div>
 
             <!--<div class="p-t-lg border-bottom relative p-o-md" v-if="data.agent_num!==0">-->
-                <div class="p-t-lg border-bottom relative p-o-md">
-                <el-tabs v-model="incomeStatus">
-                    <el-tab-pane label="播放统计" name="income"></el-tab-pane>
-                </el-tabs>
+            <div class="p-t-lg border-bottom relative p-o-md m-t-lg">
                 <el-radio-group v-model="incomeDays" size="large" class="days-select-container">
-                    <el-radio-button :label="7">本月</el-radio-button>
-                    <el-radio-button :label="14">上月</el-radio-button>
-                    <el-radio-button :label="30">其他月份</el-radio-button>
+                    <el-radio-button :label="1">本月</el-radio-button>
+                    <el-radio-button :label="2">上月</el-radio-button>
+                    <el-radio-button :label="3">其他</el-radio-button>
                 </el-radio-group>
-                <div class="chart-container p-o-lg" id="incomeChart"></div>
+                <el-select v-model="value" placeholder="请选择" class="month-select-container" v-show="incomeDays===3">
+                    <el-option
+                        v-for="item in options"
+                        :key="item.value"
+                        :disabled="item.disabled"
+                        :label="item.label"
+                        :value="item.value">
+                    </el-option>
+                </el-select>
+                <div class="chart-container" id="incomeChart"></div>
             </div>
         </div>
     </div>
@@ -61,8 +67,7 @@
                 loading: false,
 //                statisticsStatus: 'films',
 //                statisticsDays: 7,
-                incomeStatus: 'income',   //eacht 上面tabel切换
-                incomeDays: 7,   //按钮切换
+                incomeDays: 1,   //按钮切换
                 data: {
                     agent_num: 0,  //代理商初始数据
                     cinema_num: 0,  //影院总数初始数据
@@ -75,6 +80,9 @@
                     hall_status: [],
                     reservation_film: []
                 },
+                options: [],
+                value: 1,
+                month: ''
             }
         },
         methods: {
@@ -119,7 +127,7 @@
                         left: '2%', // 图表距边框的距离
                         top: '20%',
                         right: '4%',
-                        bottom: '15%',
+                        bottom: '10%',
                         containLabel: true
                     },
                     xAxis: {
@@ -140,10 +148,22 @@
                     ]
                 })
                 myChart.resize()
+            },
+            getOptions () {
+                let date = new Date()
+                this.month = date.getMonth() + 1
+                for (let i = 0; i < 12; i++) {
+                    this.options.push({
+                        label: i + 1 + '月',
+                        value: i + 1,
+                        disabled: this.month < i + 1
+                    })
+                }
             }
         },
         created () {
-            this.getChartData()
+            this.getOptions()
+            this.getChartData(this.month)
         },
 //        mounted () {
 //            let that = this
@@ -154,26 +174,13 @@
 //            })
 //        },
         watch: {
-//            statisticsDays (val) {
-//                this.getData()
-//            },
             incomeDays (val) {
-                this.getChartData(2, val)
+                let month = val === 1 ? this.month : (val === 2 ? this.month - 1 : this.value)
+                this.getChartData(month)
             },
-//            statisticsStatus (val) {
-//                if (val === 'films') {
-//                    this.getChartData(3, this.statisticsDays)
-//                } else {
-//                    this.getChartData(4, this.statisticsDays)
-//                }
-//            },
-//            statisticsDays (val) {
-//                if (this.statisticsStatus === 'films') {
-//                    this.getChartData(3, val)
-//                } else {
-//                    this.getChartData(4, val)
-//                }
-//            }
+            value (val) {
+                this.getChartData(val)
+            }
         }
     }
 </script>
@@ -183,22 +190,25 @@
         font-size: 22px;
     }
     .chart-container {
-        height: 300px;
+        height: 400px;
     }
     .days-select-container {
         position: absolute;
         top: 22px;
+        right: 130px;
+        z-index: 999;
+    }
+    .month-select-container {
+        position: absolute;
+        width: 90px;
+        top: 22px;
         right: 30px;
+        z-index: 999;;
     }
     .home-data-item {
         height: 130px;
-        /*box-shadow: 1px 1px 1px rgba(187, 187, 187, 0.75);*/
-        /*border-radius: 5px;*/
-        border: 1px solid #e9e9e9;
-        /*height: 80px;*/
         padding: 16px 10px 0px 10px;
         width: 100%;
-        /*margin-right: 1.6%;*/
         display: inline-block;
         box-shadow: 1px 0px 1px rgba(187, 187, 187, 0.75);
         border-radius: 2px;
@@ -209,7 +219,7 @@
     }
 
     .home-data-item:hover {
-        box-shadow: 1px 1px 10px rgba(187, 187, 187, 0.75);
+        box-shadow: 1px 0px 10px rgba(187, 187, 187, 0.75);
     }
 
     .hall-item-container {
