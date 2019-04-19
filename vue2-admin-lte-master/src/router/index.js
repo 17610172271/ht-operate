4b85fec1cb7b44409e9a6ee765e42f7fc6896b0d
DESKTOP-SBHJ9TT\LiKui <601353149@qq.com>
2019-04-19 09:32:14 +0800

    路由优化2

--- vue2-admin-lte-master/src/router/index.js
+++ vue2-admin-lte-master/src/router/index.js
@@ -11,7 +11,7 @@ const router = new Router({
         {
             path: '/',
             component: rs('home/index'),
-            redirect: {name: 'home'},
+            redirect: {name: 'login'},
             children: [
                 {
                     path: '/home',
